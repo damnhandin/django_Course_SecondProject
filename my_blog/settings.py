6fb0a2949d7be5c11f548469c13212a29fc43013
damnhandin <lushnikovff@gmail.com>
2024-03-03 20:00:07 +0300

    add onetomany

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'posts.apps.PostsConfig',
-    'onetoone.apps.OnetooneConfig'
+    'onetoone.apps.OnetooneConfig',
+    'onetomany.apps.OnetomanyConfig'
 ]
 
 MIDDLEWARE = [
